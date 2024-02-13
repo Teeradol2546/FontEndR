f2e16bc7bd2a325ef7e0ad0baaa59c47bc1b624e
Teeradol2546 <s6506021630020@email.kmutnb.ac.th>
2024-02-13 09:02:20 +0700

    finish

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const app = express();
 var bodyParser = require('body-parser');
 
-const base_url = "http://localhost:3000";
+const base_url = "http://node56763-teeradolnoderest.proen.app.ruk-com.cloud";
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
